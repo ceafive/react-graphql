a3d778029523f6639e92a04cb7516cdcee60c6a4
Castro Agbo <ceafive@gmail.com>
2021-01-06 16:40:44 +0000

    add package json config

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const colors = require('colors')
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
@@ -13,6 +13,14 @@ startDB()
 
 app.use(cors())
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
 
 app.listen(4000, () => {
   console.log('Listening on port 4000')
