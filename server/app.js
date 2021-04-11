7962c7fc00cbbdb98c886316204a2dd2db1aff82
Castro Agbo <ceafive@gmail.com>
2021-04-11 12:47:25 +0000

    add forgot password

--- server/app.js
+++ server/app.js
@@ -19,17 +19,20 @@ morgan.token('queryparam', (req, res) => {
   const { query } = req
   return query.userId
 })
+app.disable('x-powered-by')
 
 app.use(morgan('tiny'))
 app.use(morgan(':method :queryparam'))
 
 app.use('/graphql', express.json(), authenticate, graphqlServer)
 
+// console.log(path.join(__dirname, 'client'))
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.resolve(__dirname, 'client/build')))
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
   })
 }
 
